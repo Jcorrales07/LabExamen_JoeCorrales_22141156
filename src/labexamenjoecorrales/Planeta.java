b1f0a7d3bd5c58baa161e634cb6a21b62016aa93
Joe Corrales <corralesjoe07@gmail.com>
2022-03-25 13:32:04 -0600

    Clase Planeta

---
+++ src/labexamenjoecorrales/Planeta.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package labexamenjoecorrales;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Corra
+ */
+public class Planeta implements Serializable {
+    protected String nombre;
+    protected double tamannio;
+    protected double peso;
+    protected double coorX;
+    protected double coorY;
+
+    public Planeta(String nombre, double tamannio, double peso, double coorX, double coorY) {
+        this.nombre = nombre;
+        this.tamannio = tamannio;
+        this.peso = peso;
+        this.coorX = coorX;
+        this.coorY = coorY;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getTamannio() {
+        return tamannio;
+    }
+
+    public void setTamannio(double tamannio) {
+        this.tamannio = tamannio;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getCoorX() {
+        return coorX;
+    }
+
+    public void setCoorX(double coorX) {
+        this.coorX = coorX;
+    }
+
+    public double getCoorY() {
+        return coorY;
+    }
+
+    public void setCoorY(double coorY) {
+        this.coorY = coorY;
+    }
+
+    @Override
+    public String toString() {
+        return "Planeta{" + "nombre=" + nombre + ", tamannio=" + tamannio + ", peso=" + peso + ", coorX=" + coorX + ", coorY=" + coorY + '}';
+    }
+}
